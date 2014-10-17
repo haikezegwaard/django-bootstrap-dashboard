34dc14dfe48c63056fab9d1690289e6f8bb553f2
haikezegwaard <h.zegwaard@gmail.com>
2014-10-17 15:53:21 +0200

    initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+# Create your views here.
+def index(request):
+    return render_to_response('index.html',{'foo':'bar'},context_instance=RequestContext(request))

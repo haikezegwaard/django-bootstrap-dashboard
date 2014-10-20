f18dfd2e00998f9a3400267301e1f78ba84c3ff1
haikezegwaard <h.zegwaard@gmail.com>
2014-10-20 11:24:42 +0200

    add util.py

---
+++ dashboard/util.py
@@ -0,0 +1,18 @@
+import datetime
+from datetime import timedelta
+
+
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
+
+def unix_time_millis(dt):
+    return unix_time(dt) * 1000.0
+
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
